694292dedb66eb3986a1372de8f8116bb9fccc9d
Henk Boom <henk@henk.ca>
2009-12-29 20:57:09 -0500

    created separate ai files, undid merge problem

---
+++ scripts/factory_ai.lua
@@ -0,0 +1,9 @@
+function update()
+  local up = game.keyboard.key_held(string.byte('W'))
+  local left = game.keyboard.key_held(string.byte('A'))
+  local right = game.keyboard.key_held(string.byte('D'))
+  local space = game.keyboard.key_held(string.byte(' '))
+
+  self.ship.turn((left and 1 or 0) - (right and 1 or 0))
+  if up then self.ship.thrust() end
+end
