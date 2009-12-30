fcda9184e8b11316530965e94952c8d298ce9d3e
Renaud Bedard <renaud.bedard@gmail.com>
2009-12-29 21:44:36 -0500

    Better shaped bullets, better velocity

--- scripts/fighter_shooting.lua
+++ scripts/fighter_shooting.lua
@@ -1,5 +1,8 @@
 local blueprints = require 'blueprints'
 
+local speed = 20
+
 function shoot()
-  game.actors.new(blueprints.laser, {'transform', pos=self.transform.pos, facing=self.transform.facing})
+  local start_vel = speed * self.transform.facing + self.ship.velocity
+  game.actors.new(blueprints.laser, {'transform', pos=self.transform.pos, facing=self.transform.facing}, {'bullet', velocity=start_vel})
 end
--- scripts/frigate_shooting.lua
+++ scripts/frigate_shooting.lua
@@ -1,5 +1,8 @@
 local blueprints = require 'blueprints'
 
+local speed = 1
+
 function shoot()
-  game.actors.new(blueprints.missile, {'transform', pos=self.transform.pos})
+  local start_vel = speed * self.transform.facing + self.ship.velocity
+  game.actors.new(blueprints.missile, {'transform', pos=self.transform.pos}, {'bullet', velocity=start_vel})
 end
