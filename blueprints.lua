2281a14155db5b1002349ec22556be4b5877a6f6
Matthew Gallant <matthew@dyn-217-176.wireless.concordia.ca>
2009-12-29 22:23:09 -0500

    Adding player and hit points to ships.

--- blueprints.lua
+++ blueprints.lua
@@ -6,28 +6,28 @@ local v2 = require 'dokidoki.v2'
 fighter = game.make_blueprint('fighter',
   {'transform', scale_x=2, scale_y=2},
   {'sprite', resource='fighter_sprite'},
-  {'ship', turn_speed=0.05, accel=0.2},
+  {'ship', player=1, turn_speed=0.05, accel=0.2, hit_points=50},
   {'fighter_shooting'},
   {'fighter_ai'})
 
 bomber = game.make_blueprint('bomber',
   {'transform', scale_x=4, scale_y=4},
   {'sprite', resource='fighter_sprite'},
-  {'ship', turn_speed=0.02, accel=0.1},
+  {'ship', player=1, turn_speed=0.02, accel=0.1, hit_points=200},
   {'bomber_shooting'},
   {'bomber_ai'})
 
 frigate = game.make_blueprint('frigate',
   {'transform', scale_x=8, scale_y=8},
   {'sprite', resource='fighter_sprite'},
-  {'ship', turn_speed=0.01, accel=0.02},
+  {'ship', player=1, turn_speed=0.01, accel=0.02, hit_points=500},
   {'frigate_shooting'},
   {'frigate_ai'})
 
 factory = game.make_blueprint('factory',
   {'transform', scale_x=30, scale_y=30},
   {'sprite', resource='fighter_sprite'},
-  {'ship', turn_speed=0.001, accel=0.005},
+  {'ship', player=1, turn_speed=0.001, accel=0.005, hit_points=10000},
   {'factory_ai'})
   
 laser = game.make_blueprint('laser',
