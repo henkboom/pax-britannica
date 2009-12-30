0562e0941629fc8527b1b6c96edea43533d69f14
Henk Boom <henk@henk.ca>
2009-12-29 20:29:38 -0500

    changed scales/speeds of ship types

--- blueprints.lua
+++ blueprints.lua
@@ -6,25 +6,25 @@ local v2 = require 'dokidoki.v2'
 fighter = game.make_blueprint('fighter',
   {'transform', scale_x=2, scale_y=2},
   {'sprite', resource='fighter_sprite'},
-  {'ship', turn_speed=math.pi/60, accel=0.15},
+  {'ship', turn_speed=0.05, accel=0.2},
   {'wasd_ship_control'})
 
 bomber = game.make_blueprint('bomber',
-  {'transform', scale_x=2, scale_y=2},
+  {'transform', scale_x=4, scale_y=4},
   {'sprite', resource='fighter_sprite'},
-  {'ship', turn_speed=math.pi/60, accel=0.15},
+  {'ship', turn_speed=0.02, accel=0.1},
   {'wasd_ship_control'})
 
 frigate = game.make_blueprint('frigate',
-  {'transform', scale_x=2, scale_y=2},
+  {'transform', scale_x=8, scale_y=8},
   {'sprite', resource='fighter_sprite'},
-  {'ship', turn_speed=math.pi/60, accel=0.15},
+  {'ship', turn_speed=0.01, accel=0.02},
   {'wasd_ship_control'})
 
 factory = game.make_blueprint('factory',
-  {'transform', scale_x=2, scale_y=2},
+  {'transform', scale_x=30, scale_y=30},
   {'sprite', resource='fighter_sprite'},
-  {'ship', turn_speed=math.pi/60, accel=0.15},
+  {'ship', turn_speed=0.001, accel=0.005},
   {'wasd_ship_control'})
 
 return get_module_exports()
