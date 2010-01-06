01e62eb12399a05e5a91883c2c2985c20169ab2d
Henk Boom <henk@henk.ca>
2010-01-06 02:21:05 -0500

    reworked fighter ai

--- scripts/fighter_ai.lua
+++ scripts/fighter_ai.lua
@@ -1,31 +1,70 @@
 local v2 = require 'dokidoki.v2'
 
-local shots = 3
+-- shot range
+local shot_distance = 350
+-- try to stay this far away
+local clear_distance = 200
+
+local shot_cooldown_time = 6
+local shot_capacity = 5
+local shot_reload_rate = 2/60
+
+local shots = shot_capacity
+local cooldown = 0
+
+-- true when we've shot everything and want to make a distance, false means
+-- we're approaching to attack
+local clearing = false
+
+local target = false
+
+local function retarget ()
+  target = game.targeting.get_nearest_of_type(self, 'factory')
+end
 
 function update()
-  local target = game.targeting.get_nearest_of_type(self, 'factory')
+  if not target then
+    retarget()
+  end
 
   if target then
-    -- Move away from target if out of shots
-    if shots > 1 then
-        self.ship.go_towards(target.transform.pos, true)
-    else
+    local to_target = target.transform.pos - self.transform.pos
+    local dist_squared = v2.sqrmag(to_target)
+
+    if clearing then
+      local too_close = dist_squared < clear_distance^2
+      if too_close then
         self.ship.go_away(target.transform.pos, true)
-    end
-    
-    target_distance = v2.mag(target.transform.pos - self.transform.pos)
-    target_direction = v2.norm(target.transform.pos - self.transform.pos)
-    facing_direction = v2.norm(self.transform.facing)
-    
-    -- Only shoot when facing target and in range
-    if shots > 1 and target_distance < 140 and v2.dot(facing_direction, target_direction) > 0.99 then
-        self.fighter_shooting.shoot()
-        shots = shots - 1
+      else
+        self.ship.thrust()
+      end
+
+      if shots == shot_capacity and not too_close then
+        clearing = false
+      end
+    else
+      self.ship.go_towards(target.transform.pos, true)
+
+      -- maybe shoot
+      cooldown = math.max(0, cooldown - 1)
+      if cooldown == 0 then
+        local facing = self.transform.facing
+        if dist_squared <= shot_distance * shot_distance
+           and v2.dot(to_target, facing) > 0
+           and v2.dot(to_target, facing)^2 > 0.9 * dist_squared then
+          self.fighter_shooting.shoot()
+          shots = shots - 1
+          cooldown = shot_cooldown_time
+        end
+      end
+
+      -- if out of shots then clear out
+      if shots < 1 then
+        clearing = true
+      end
     end
   end
   
   -- Gradually recharge shots
-  if shots < 3 then
-    shots = math.min(shots + 0.04, 3)
-  end
+  shots = math.min(shots + shot_reload_rate, shot_capacity)
 end
