bd52137dd35987e6ae39c9fb3587b267b87990fb
Henk Boom <henk@henk.ca>
2010-01-06 02:41:32 -0500

    refactored cooldown/ammo code into fighter_shooting

--- scripts/fighter_ai.lua
+++ scripts/fighter_ai.lua
@@ -1,26 +1,23 @@
 local v2 = require 'dokidoki.v2'
 
 -- shot range
-local shot_distance = 350
--- try to stay this far away
-local clear_distance = 200
-
-local shot_cooldown_time = 6
-local shot_capacity = 5
-local shot_reload_rate = 2/60
-
-local shots = shot_capacity
-local cooldown = 0
+local shot_range = 350
+-- try to stay this far away when you're out of ammo
+local run_distance = 200
 
 -- true when we've shot everything and want to make a distance, false means
 -- we're approaching to attack
-local clearing = false
+local running = false
 
 local target = false
 
 local function retarget ()
   target = game.targeting.get_nearest_of_type(self, 'factory')
 end
+
+-- basically, the behaviour is to go towards the target and attack it, then
+-- when you're out of ammo run away until you've got your ammo back and have
+-- gone a certain distance
 
 function update()
   if not target then
@@ -31,40 +28,39 @@ function update()
     local to_target = target.transform.pos - self.transform.pos
     local dist_squared = v2.sqrmag(to_target)
 
-    if clearing then
-      local too_close = dist_squared < clear_distance^2
+    if running then
+      -- run away until you have full ammo and are far enough away
+
+      local too_close = dist_squared < run_distance^2
+      -- if you're too close to the target then turn away
       if too_close then
         self.ship.go_away(target.transform.pos, true)
       else
         self.ship.thrust()
       end
 
-      if shots == shot_capacity and not too_close then
-        clearing = false
+      if self.fighter_shooting.is_reloaded() and not too_close then
+        running = false
       end
     else
+      -- go towards the target and attack!
+
       self.ship.go_towards(target.transform.pos, true)
 
       -- maybe shoot
-      cooldown = math.max(0, cooldown - 1)
-      if cooldown == 0 then
+      if self.fighter_shooting.is_ready_to_shoot() then
         local facing = self.transform.facing
-        if dist_squared <= shot_distance * shot_distance
-           and v2.dot(to_target, facing) > 0
-           and v2.dot(to_target, facing)^2 > 0.9 * dist_squared then
+        if dist_squared <= shot_range * shot_range
+          and v2.dot(to_target, facing) > 0
+          and v2.dot(to_target, facing)^2 > 0.97 * dist_squared then
           self.fighter_shooting.shoot()
-          shots = shots - 1
-          cooldown = shot_cooldown_time
         end
       end
 
-      -- if out of shots then clear out
-      if shots < 1 then
-        clearing = true
+      -- if out of shots then run away
+      if self.fighter_shooting.is_empty() then
+        running = true
       end
     end
   end
-  
-  -- Gradually recharge shots
-  shots = math.min(shots + shot_reload_rate, shot_capacity)
 end
--- scripts/fighter_shooting.lua
+++ scripts/fighter_shooting.lua
@@ -1,8 +1,40 @@
 local blueprints = require 'blueprints'
 
-local speed = 20
+local bullet_speed = 20
+local shot_cooldown_time = 6
+local shot_capacity = 5
+local shot_reload_rate = 2/60
+
+local shots = shot_capacity
+local cooldown = 0
+
+function is_empty()
+  return shots < 1
+end
+
+function is_reloaded()
+  return shots == shot_capacity
+end
+
+function is_cooled_down()
+  return cooldown == 0
+end
+
+function is_ready_to_shoot()
+  return is_cooled_down() and not is_empty()
+end
+
+function update()
+  cooldown = math.max(0, cooldown - 1)
+  shots = math.min(shots + shot_reload_rate, shot_capacity)
+end
 
 function shoot()
-  local start_vel = speed * self.transform.facing + self.ship.velocity
-  game.actors.new(blueprints.laser, {'transform', pos=self.transform.pos, facing=self.transform.facing}, {'bullet', velocity=start_vel})
+  if cooldown == 0 and shots >= 1 then
+    shots = shots - 1
+    cooldown = shot_cooldown_time
+
+    local start_vel = bullet_speed * self.transform.facing + self.ship.velocity
+    game.actors.new(blueprints.laser, {'transform', pos=self.transform.pos, facing=self.transform.facing}, {'bullet', velocity=start_vel})
+  end
 end
