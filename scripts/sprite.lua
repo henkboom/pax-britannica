adca5a2c716a8148523225b758d45824c5e04c9f
Henk Boom <henk@henk.ca>
2010-01-06 02:48:05 -0500

    sprites can be coloured

--- scripts/sprite.lua
+++ scripts/sprite.lua
@@ -1,5 +1,7 @@
 local gl = require 'gl'
 
+color = color or false
+
 if not image then
   if resource then
     image = game.resources[resource]
@@ -27,7 +29,9 @@ function draw()
   gl.glRotated(180/math.pi * math.atan2(f.y, f.x), 0, 0, 1)
   gl.glScaled(self.transform.scale_x, self.transform.scale_y, 0)
 
+  if color then gl.glColor4d(color[1], color[2], color[3], color[4] or 1) end
   image:draw()
+  if color then gl.glColor3d(1, 1, 1) end
 
   gl.glPopMatrix()
 end
