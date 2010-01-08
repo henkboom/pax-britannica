590553ed385b0398d0fbab7e12c72facb9bb84b3
Henk Boom <henk@henk.ca>
2010-01-07 21:39:41 -0500

    health only shows when you hit `

--- components/tracing.lua
+++ components/tracing.lua
@@ -2,29 +2,35 @@ local gl = require 'gl'
 
 local tracers = {}
 
+local function in_debug_mode()
+  return game.keyboard.key_held(string.byte('`'))
+end
+
 function trace_bar(pos, progress)
-  table.insert(tracers, game.actors.new_generic('arrow_trace', function ()
-    function draw()
-      local left = -10 + pos.x
-      local right = 10 + pos.x
-      local bottom = -1 + pos.y
-      local top = 1 + pos.y
-      local mid = left + (right - left) * progress
-      gl.glBegin(gl.GL_QUADS)
-      gl.glColor3d(1, 0, 0)
-      gl.glVertex2d(left, bottom)
-      gl.glVertex2d(right, bottom)
-      gl.glVertex2d(right, top)
-      gl.glVertex2d(left, top)
-      gl.glColor3d(0, 1, 0)
-      gl.glVertex2d(left, bottom)
-      gl.glVertex2d(mid, bottom)
-      gl.glVertex2d(mid, top)
-      gl.glVertex2d(left, top)
-      gl.glEnd()
-      gl.glColor3d(1, 1, 1)
-    end
-  end))
+  if in_debug_mode() then
+    table.insert(tracers, game.actors.new_generic('arrow_trace', function ()
+      function draw()
+        local left = -10 + pos.x
+        local right = 10 + pos.x
+        local bottom = -1 + pos.y
+        local top = 1 + pos.y
+        local mid = left + (right - left) * progress
+        gl.glBegin(gl.GL_QUADS)
+        gl.glColor3d(1, 0, 0)
+        gl.glVertex2d(left, bottom)
+        gl.glVertex2d(right, bottom)
+        gl.glVertex2d(right, top)
+        gl.glVertex2d(left, top)
+        gl.glColor3d(0, 1, 0)
+        gl.glVertex2d(left, bottom)
+        gl.glVertex2d(mid, bottom)
+        gl.glVertex2d(mid, top)
+        gl.glVertex2d(left, top)
+        gl.glEnd()
+        gl.glColor3d(1, 1, 1)
+      end
+    end))
+  end
 end
 
 game.actors.new_generic('tracer_cleanup', function ()
