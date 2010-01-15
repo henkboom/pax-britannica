ef415d1b17cf324646c0a825edfa960e43e76037
Matthew Gallant <matthew@gangles.ca>
2010-01-14 22:51:34 -0500

    More statistic tracking.

--- components/log.lua
+++ components/log.lua
@@ -4,7 +4,17 @@ local blueprints = require 'blueprints'
 local spawn = { fighter=0, bomber=0, frigate=0 }
 local death = { fighter=0, bomber=0, frigate=0 }
 local damage_given = { fighter=0, bomber=0, frigate=0 }
-local damage_received = { fighter=0, bomber=0, frigate=0 }
+local fighter_damaged_by = { fighter=0, bomber=0, frigate=0 }
+local bomber_damaged_by = { fighter=0, bomber=0, frigate=0 }
+local frigate_damaged_by = { fighter=0, bomber=0, frigate=0 }
+local factory_damaged_by = { fighter=0, bomber=0, frigate=0 }
+local damage_received =
+{
+  fighter = fighter_damaged_by,
+  bomber = bomber_damaged_by,
+  frigate = frigate_damaged_by,
+  factory = factory_damaged_by
+}
 local accuracy_hits = {fighter=0, bomber=0, frigate=0}
 local accuracy_misses = {fighter=0, bomber=0, frigate=0}
 local frame_count = 0
@@ -38,12 +48,12 @@ function record_hit(receiver, bullet)
   
   damage_given[giver] = damage_given[giver] or 0
   damage_given[giver] = damage_given[giver] + real_damage
-  
-  damage_received[receiver.blueprint.name] = damage_received[receiver.blueprint.name] or 0
-  damage_received[receiver.blueprint.name] = damage_received[receiver.blueprint.name] + real_damage
-                                             
+
   accuracy_hits[giver] = accuracy_hits[giver] or 0
   accuracy_hits[giver] = accuracy_hits[giver] + 1
+
+  damage_received[receiver.blueprint.name][giver] = damage_received[receiver.blueprint.name][giver] or 0
+  damage_received[receiver.blueprint.name][giver] = damage_received[receiver.blueprint.name][giver] + real_damage
 end
 
 function record_miss(bullet)
@@ -61,11 +71,18 @@ function print_stats()
   print('==         GAME STATISTICS         ==')
   print('=====================================')
   print('Time Elapsed:')    print("", string.format("%.3f seconds", frame_count / 60))
-  print('Constructed:')     for k,v in pairs(spawn) do print("    ",string.upper(k),v) end
-  print('Destroyed:')       for k,v in pairs(death) do print("    ",string.upper(k),v) end
-  print('Damage Given:')    for k,v in pairs(damage_given) do print("    ",string.upper(k),v) end
-  print('Damage Received:') for k,v in pairs(damage_received) do print("    ",string.upper(k),v) end
-  
+  print('Constructed:')     for k,v in pairs(spawn) do print('',string.upper(k),v) end
+  print('Destroyed:')       for k,v in pairs(death) do print('',string.upper(k),v) end
+  print('Damage Given:')    for k,v in pairs(damage_given) do print('',string.upper(k),v) end
+  print('Damage Received:')
+    print('','Fighter damaged by')
+    for k,v in pairs(damage_received['fighter']) do print('','',string.upper(k),v) end
+    print('','Bomber damaged by')
+    for k,v in pairs(damage_received['bomber']) do print('','',string.upper(k),v) end
+    print('','Frigate damaged by')
+    for k,v in pairs(damage_received['frigate']) do print('','',string.upper(k),v) end
+    print('','Factory damaged by')
+    for k,v in pairs(damage_received['factory']) do print('','',string.upper(k),v) end
   print('Accuracy:')
   for k,v in pairs(accuracy_hits) do
     accuracy = v / (v + accuracy_misses[k]) * 100
