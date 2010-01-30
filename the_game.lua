a420ca1a3cb8c1e0d4d901089baf6b81beb47f52
Henk Boom <henk@henk.ca>
2010-01-30 16:53:24 -0500

    disabled background clearing

--- the_game.lua
+++ the_game.lua
@@ -13,13 +13,15 @@ function make ()
     {'draw_setup', 'draw', 'particle_draw', 'fade_draw'},
     function (game)
       math.randomseed(os.time())
-  
+
       game.init_component('exit_handler')
       game.init_component('keyboard')
       game.init_component('opengl_2d')
       game.opengl_2d.width = 1024
       game.opengl_2d.height = 768
-  
+      -- disable clearing, since we draw our own background
+      game.opengl_2d.background_color = false
+
       game.init_component('constants')
       game.init_component('collision')
       game.init_component('resources')
@@ -29,17 +31,14 @@ function make ()
       game.init_component('fast_forward')
       game.init_component('the_one_button')
       game.init_component('particles')
-      
+
       function game.exit_handler.on_close()
         game.log.print_stats()
         kernel.abort_main_loop()
       end
-  
-      game.actors.new(blueprints.background,
-        {'transform', pos=v2(0, 0)})
-  
+
+      game.actors.new(blueprints.background)
       game.actors.new(blueprints.background_fx)
-  
       game.actors.new(blueprints.game_flow)
     end)
 end
