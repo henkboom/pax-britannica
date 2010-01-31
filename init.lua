229673a8209b2c267803cd6447f740b02ea4a697
Henk Boom <henk@henk.ca>
2010-01-31 02:22:00 -0500

    fullscreen by default (--windowed for window mode)

--- init.lua
+++ init.lua
@@ -4,5 +4,14 @@ local kernel = require 'dokidoki.kernel'
 
 local the_game = require 'the_game'
 
+local args = {}
+for _, a in ipairs(arg) do
+  args[a] = true
+end
+
+if not args['--windowed'] then
+  kernel.set_fullscreen(true)
+end
+
 kernel.set_video_mode(1024, 768)
 kernel.start_main_loop(the_game.make())
